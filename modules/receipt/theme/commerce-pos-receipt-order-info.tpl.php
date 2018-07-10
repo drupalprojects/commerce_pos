f86f265d05130184560aad79ec774bd5d9aecef7
git <git@3569777.no-reply.drupal.org>
2018-07-10 14:03:55 -0700

    Issue #2957925 by gmem, smccabe, sharif.tanveer: Remove Customer name from POS receipt

--- modules/receipt/theme/commerce-pos-receipt-order-info.tpl.php
+++ modules/receipt/theme/commerce-pos-receipt-order-info.tpl.php
@@ -24,7 +24,7 @@
 
   <div class="order-number"><?php print t('Order:'); ?> <span><?php print $order->order_id ? $order->order_id : t('New Order'); ?></span></div>
 
-  <?php if (!empty($order->uid)) { ?>
+  <?php if (!empty($order->uid) && $transaction->data['customer printname'] !== 0) { ?>
     <div class="order-customer"><?php print t('Customer:'); ?> <span><?php print format_username(user_load($order->uid)); ?></span></div>
   <?php } ?>
 </div>
